f344a6252460fe812c02a5d93adb4ed3544151d9
chandan <csc3641701>
2024-05-08 10:38:57 +0530

    first commit

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const dotenv = require('dotenv')
 dotenv.config({path:'./config.env'})
 
 // const DB=`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.o4ffhpq.mongodb.net/${process.env.DATABASE}`
-const DB=`mongodb://admin:Ysec123@172.20.10.104:27017/?`
+const DB=`mongodb+srv://chandanuidevops:Chandan@123@cluster0.wtuzfpj.mongodb.net/`
 mongoose.connect(DB,{
     useNewUrlParser: true,
     useCreateIndex: true,
